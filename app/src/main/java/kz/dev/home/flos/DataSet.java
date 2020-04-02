f1f465d661b97cd5924780f790501475c213eae9
Rykov_D <drycov@gmail.com>
2020-04-03 02:40:03 +0600

    FFFF

---
+++ app/src/main/java/kz/dev/home/flos/DataSet.java
@@ -0,0 +1,6 @@
+package kz.dev.home.flos;
+
+public class DataSet {
+    static String[] title_Array = {"JellyBean", "Kitkat", "Lollipop", "Marshmallow", "Nougat", "Oreo"};
+    static String[] desc_Array = {"4.1", "4.4", "5.0","6.0","7.0", "8.0"};
+}
